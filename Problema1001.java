cd3cde1bf94b61fad8f7b2d7713b39818bbf629b
Rafael Walker Batista <rafaelwalker@icloud.com>
2016-04-15 17:49:15 -0300

    Finish

---
+++ Problema1001.java
@@ -0,0 +1,20 @@
+package uriproblems;
+
+import java.util.Scanner;
+
+public class Problema1001 {
+    public static void main(String[] args) {
+        int A;
+        int B;
+        int X;
+        
+        Scanner teclado = new Scanner (System.in);
+        
+        A = teclado.nextInt();
+        B = teclado.nextInt();
+        
+        X = A+B;
+        
+        System.out.println("X = " +X);
+    }
+}
